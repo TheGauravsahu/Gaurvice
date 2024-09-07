23421ae3a33b9d401f73b409cbf06986319ea277
Gaurav Sahu <itsgauravsahu7+github@gmail.com>
2024-09-07 09:31:10 +0530

    made booking page responsive

--- components/BookingHistory.tsx
+++ components/BookingHistory.tsx
@@ -9,7 +9,7 @@ const BookingHistory = ({ bookings }: { bookings: any }) => {
         bookings.map((item: any, index: any) => (
           <div
             key={index}
-            className="shadow-sm border w-full h-[170px] mx-auto rounded-lg overflow-hidden flex items=center  gap-4"
+            className="shadow-sm border w-full lg:h-[170px] mx-auto rounded-lg overflow-hidden flex flex-col md:flex-row items=center  gap-4"
           >
             <div>
               <Image
@@ -17,41 +17,45 @@ const BookingHistory = ({ bookings }: { bookings: any }) => {
                 alt={item.businessList?.name}
                 width={200}
                 height={400}
-                className="rounded-lg w-fit object-cover"
+                className="rounded-lg lg:w-fit w-full object-cover"
               />
             </div>
+            <div className="flex flex-wrap">
+              <div className="p-4">
+                <h2 className="font-semibold text-2xl mb-4">Details</h2>
+                <hr />
+                <h2 className="font-medium text-lg mt-2">
+                  {item.businessList?.name}
+                </h2>
+                <p>{item.businessList?.address}</p>
+              </div>
 
-            <div className="p-4">
-              <h2 className="font-semibold text-2xl mb-4">Details</h2>
-              <hr />
-              <h2 className="font-medium text-lg mt-2">
-                {item.businessList?.name}
-              </h2>
-              <p>{item.businessList?.address}</p>
-            </div>
-            <div className="p-4">
-              <h2 className="font-semibold text-2xl mb-4">Timings</h2>
-              <hr />
+              <div className="p-4">
+                <h2 className="font-semibold text-2xl mb-4">Timings</h2>
+                <hr />
 
-              <p className="font-medium text-lg">{item?.date}</p>
-              <p>{item?.time}</p>
-            </div>
-            <div className="p-4">
-              <h2 className="font-semibold text-2xl mb-4">Contact</h2>
-              <hr />
+                <p className="font-medium text-lg">{item?.date}</p>
+                <p>{item?.time}</p>
+              </div>
 
-              <p className="font-medium text-lg mt-2">
-                {item.businessList?.contactPerson}
-              </p>
-              <p>{item.businessList?.email}</p>
-            </div>
-            <div className="p-4">
-              <h2 className="font-semibold text-2xl mb-4">Status</h2>
-              <hr />
+              <div className="p-4">
+                <h2 className="font-semibold text-2xl mb-4">Contact</h2>
+                <hr />
 
-              <p className="capitalize font-medium text-lg mt-2">
-                {item?.bookingStatus}
-              </p>
+                <p className="font-medium text-lg mt-2">
+                  {item.businessList?.contactPerson}
+                </p>
+                <p>{item.businessList?.email}</p>
+              </div>
+
+              <div className="p-4">
+                <h2 className="font-semibold text-2xl mb-4">Status</h2>
+                <hr />
+
+                <p className="capitalize font-medium text-lg mt-2">
+                  {item?.bookingStatus}
+                </p>
+              </div>
             </div>
           </div>
         ))
